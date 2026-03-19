690beab893b15256e9a4d9ca119588d50131f5d2
xingsdong <xingsdong@hust.edu.cn>
2026-03-19 14:25:48 +0800

    任意位置插入法

--- main.cpp
+++ main.cpp
@@ -5,15 +5,36 @@ struct node
     int data;
     node* next;
 };
-
+node* head;//global viarient
 void Insert(node** pointertohead,int x)
 {
     node* temp = new node();
     temp->data = x;
     temp->next = *pointertohead;
     *pointertohead = temp;
-
-
+}
+void Insert_Any(int data,int n )
+/*@数据 @位置
+ *
+ */
+{
+    node* temp1 = new node();
+    temp1->data = data;
+    if (n==1) //改变头指针和temp1的位置
+    {
+        temp1->next = head;
+        head = temp1;
+    }
+    else
+    {
+        node* temp2 =head;//to find the nth node
+        for (int i=0;i<n-2;i++)
+        {
+            temp2 = temp2->next;
+        }
+            temp1->next = temp2->next;
+            temp2->next = temp1;
+    }
 }
 void Print(node* temp)
 {
@@ -28,16 +49,11 @@ void Print(node* temp)
 }
 int main()
 {
-    node* head= NULL;//head pointer
-    cout<<"how many numbers in list"<<endl;
-    int num,x;
-    cin>>num;
-    for (int i=0;i<num;i++)
-    {
-        cout<<"enter the number"<<endl;
-        cin>>x;//new number
-        Insert(&head,x);
-        Print(head);
-    }
+    head= NULL;//head pointer
+    Insert_Any(7,1);//7
+    Insert_Any(6,1);//67
+    Insert_Any(2,2);//627
+    Print(head);
+
 
 }
